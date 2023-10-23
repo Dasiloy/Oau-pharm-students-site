71bd2f944381791b85c950b5bd6ef3464d9d61a9
Toluwalase Ogunsola <93495428+Captain-awesome619@users.noreply.github.com>
2023-10-23 04:38:40 +0100

    feed a pansite

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-
+import Image from 'next/image'
 import { health } from './data'
 import { events } from './data'
 import { special } from './data'
@@ -9,14 +9,12 @@ import { brands } from './data'
 import { Poppins } from 'next/font/google'
 const Articles = () => {
   return (
-    <div>
-
-      <div className='flex flex-col items-center justify-center gap-12'>
-     <div className='flex items-center justify-center bg-[url("/assests/rectangle9.png")] bg-cover bg-center h-96 w-full'>
-     <h1 className='font-bold text-[56px] text-white'>ARTICLES</h1>
+      <div className='flex flex-col items-center justify-center gap-[6rem]'>
+     <div className='flex items-center justify-center bg-[url("/assests/rectangle9.png")] bg-cover bg-center h-[200px] lg:h-[500px] w-full'>
+     <h1 className='font-extrabold text-[24px] lg:text-[56px] flex items-center justify-center text-white'>ARTICLES</h1>
      </div>
      <div className='flex flex-col  items-center justify-center'>
-      <h2 className='font-bold text-[40px] text-[#ECBB55]'>Our Articles</h2>
+      <h2 className='font-bold text-[20px] lg:text-[40px] text-[#ECBB55]'>Our Articles</h2>
       < p className='text-dark-dark-100 text-[18px] font-normal items-center justify-center text-center w-[80%]'>PANS OAU makes an effort to ensure that her members are not only bookworms,
          but also have a good time. So, at PANS OAU, we organize a variety of social events such as hangouts, movie nights,
         parties, and other programs aimed at fostering social relationships.</p>
@@ -25,14 +23,14 @@ const Articles = () => {
      <div className='grid justify-center items-center px-[2rem] gap-4'>
      <h1 className='font-bold  text-[32px] text-dark-dark-100 '>Health/Sciences</h1>
      <div className=' flex flex-col items-center justify-center gap-[3rem]'>
-<div className='grid grid-cols-2 items-center justify-center gap-8 ' >
+<div className='grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-8 ' >
   {health.map((hlt)=>( <div className=''>
 <div className=' flex flex-col gap-4'>
-<img src={hlt.artimg} alt='coverimg' className='' />
+<Image src={hlt.artimg} alt='coverimg' height={500} width={650} className='' />
 <h2 className="font-semibold text-primary-500 x]text-[20px] cursor-pointer">{hlt.header}</h2>
 <p className='font-normal text-[14px] text-dark-dark-100 w-[90%]'>{hlt.text}</p>
 <div className='flex flex-row  gap-2'>
-  <img src={hlt.author} alt="author's picture"/>
+  <Image src={hlt.author} width={50} height={50} alt="author's picture"/>
   <div className='flex flex-col '>
 <h5 className='font-bold text-dark-dark-100 text-[16px]'>{hlt.authorname}</h5>
 <h5 className='font-normal text-dark-dark-100 text-[12px]' >{hlt.role}</h5>
@@ -41,13 +39,13 @@ const Articles = () => {
 </div>
   </div>))}
 </div>
-<button className='flex items-center justify-center rounded-md w-[210px] bg-primary-500 h-[65px] cursor-pointer font-semibold text-white text-[16px] ease-in-out duration-500 border-[1px] hover:border-primary-500 hover:bg-transparent hover:text-primary-500'>Read More</button>
+<button className=' rounded-md w-[210px] bg-primary-500 h-[65px] cursor-pointer font-semibold text-white text-[16px] ease-in-out duration-500 border-[1px] hover:border-primary-500 hover:bg-transparent hover:text-primary-500'>Read More</button>
 </div>
 </div>
-     <div className='grid justify-center items-center px-[2rem] gap-4'>
+<div className='grid justify-center items-center px-[2rem] gap-4'>
      <h1 className='font-bold  text-[32px] text-dark-dark-100 '>News/Events</h1>
      <div className=' flex flex-col items-center justify-center gap-[3rem]'>
-<div className='grid grid-cols-2 items-center justify-center gap-8 ' >
+<div className='grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-8 ' >
   {events.map((eve)=>( <div className=''>
 <div className=' flex flex-col gap-4'>
 <img src={eve.artimg} alt='coverimg' className='' />
@@ -69,14 +67,14 @@ const Articles = () => {
      <div className='grid justify-center items-center px-[2rem] gap-4'>
      <h1 className='font-bold  text-[32px] text-dark-dark-100 '>News/Events</h1>
      <div className=' flex flex-col items-center justify-center gap-[3rem]'>
-<div className='grid grid-cols-2 items-center justify-center gap-8 ' >
+<div className='grid grid-cols-1 lg:grid-cols-2 items-center justify-center gap-8 ' >
   {special.map((spc)=>( <div className=''>
 <div className=' flex flex-col gap-4'>
-<img src={spc.artimg} alt='coverimg' className='' />
+<Image height={500} src={spc.artimg} width={650} alt='coverimg' className='' />
 <h2 className="font-semibold text-primary-500 x]text-[20px] cursor-pointer">{spc.header}</h2>
 <p className='font-normal text-[14px] text-dark-dark-100 w-[90%]'>{spc.text}</p>
 <div className='flex flex-row  gap-2'>
-  <img src={spc.author} alt="author's picture"/>
+  <Image height={50} src={spc.author} width={50} alt="author's picture"/>
   <div className='flex flex-col '>
 <h5 className='font-bold text-dark-dark-100 text-[16px]'>{spc.authorname}</h5>
 <h5 className='font-normal text-dark-dark-100 text-[12px]' >{spc.role}</h5>
@@ -91,14 +89,14 @@ const Articles = () => {
      <div className='grid justify-center items-center px-[2rem] gap-4'>
      <h1 className='font-bold  text-[32px] text-dark-dark-100 '>More Stories</h1>
      <div className=' flex flex-col items-center justify-center gap-[3rem]'>
-<div className='grid grid-cols-3 items-center justify-center gap-8 ' >
+<div className='grid lg:grid-cols-3 grid-row  items-center justify-center gap-8 ' >
   {more.map((mor)=>( <div className=''>
 <div className=' flex flex-col gap-4'>
-<img src={mor.artimg} alt='coverimg' className='' />
+<Image height={500} src={mor.artimg} alt='coverimg' width={500} className='' />
 <h2 className="font-semibold text-primary-500 x]text-[20px] cursor-pointer">{mor.header}</h2>
 <p className='font-normal text-[14px] text-dark-dark-100 w-[90%]'>{mor.text}</p>
-<div className='flex flex-row  gap-2'>
-  <img src={mor.author} alt="author's picture"/>
+<div className='flex lg:flex-row  gap-2'>
+  <Image height={50} src={mor.author} width={50} alt="author's picture"/>
   <div className='flex flex-col '>
 <h5 className='font-bold text-dark-dark-100 text-[16px]'>{mor.authorname}</h5>
 <h5 className='font-normal text-dark-dark-100 text-[12px]' >{mor.role}</h5>
@@ -109,16 +107,16 @@ const Articles = () => {
 </div>
 </div>
 </div>
-<div className='grid grid-cols-4 my-[5rem]'>
+<div className='grid grid-cols-4 my-[2.5rem] lg:my-[5rem] gap-2  lg:gap-6'>
   {brands.map((brand) =>(
-    <div className=''>
-      <img src={brand.brand} alt='brands' />
+    <div className='flex flex-row '>
+      <Image  height={300} width={300}  src={brand.brand} alt='brands'    />
     </div>
   ))}
 </div>
 
 </div>
-    </div>
+
   )
 }
 
---
+++ src/app/projects/data.tsx
@@ -0,0 +1,50 @@
+export const promotions =[
+    {
+        "promotion" : "/assests/reactangle15.png"
+    },
+    {
+        "promotion" : "/assests/reactangle16.png"
+    },
+    {
+        "promotion" : "/assests/reactangle17.png"
+    }
+    ]
+export const foodstuff =[
+    {
+        "promotion" : "/assests/rectangle20.png"
+    },
+    {
+        "promotion" : "/assests/rectangle21.png"
+    },
+    {
+        "promotion" : "/assests/rectangle22.png"
+    },
+    {
+        "promotion" : "/assests/rectangle23.png"
+    },
+    {
+        "promotion" : "/assests/rectangle24.png"
+    },
+    {
+        "promotion" : "/assests/rectangle25.png"
+    }
+    ]
+
+    export const slide1 =[
+        {
+            "promotion" : "/assests/rectangle26.png"
+        },
+        {
+            "promotion" : "/assests/rectangle27.png"
+        },
+        
+        ]
+    export const slide2 =[
+        {
+            "promotion" : "/assests/rectangle28.png"
+        },
+        {
+            "promotion" : "/assests/rectangle29.png"
+        },
+
+        ]
