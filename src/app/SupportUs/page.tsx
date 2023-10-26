f6f1b84da7bd6710c4e0da5a5a569c0b5507d279
heywhyrex <oyeniyan2000@gmail.com>
2023-10-26 15:47:53 +0100

    added a map image

--- src/app/SupportUs/page.tsx
+++ src/app/SupportUs/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroPageCard from '../components/HeroPages/HeroPage';
 import WhyUs from './components/WhyNeed';
 import Reach from './components/Reach';
+import Image from 'next/image';
 function SupportUs() {
   const headings = 'Support Us';
   return (
@@ -9,10 +10,15 @@ function SupportUs() {
       <HeroPageCard headings={headings} />
       <WhyUs />
       <Reach />
+      <div className='mt-20 flex justify-center'>
+        <Image src='/map.png' 
+        alt='map'
+        width={1900}
+        height={500}
+        />
+      </div>
     </div>
   );
 }
 
 export default SupportUs;
-
-
