6b5ca269724c13db397fe756ded83bcca8876564
heywhyrex <oyeniyan2000@gmail.com>
2023-10-28 09:50:14 +0100

    made some changes at the hero session

--- src/app/Homepage/Hero.js
+++ src/app/Homepage/Hero.js
@@ -12,10 +12,7 @@ function Hero() {
   return (
     <div className=" ">
       <div className="">
-        <div
-          style={backgroundImageStyle}
-          className="md:flex items-center grid grid-cols-1  "
-        >
+        <div className="md:flex items-center  bg-[url('/backgroundP.png')] grid-cols-1 bg-cover bg-center h-[300px] lg:h-[840px] w-full  ">
           <div className=" text-center md:text-left md:mt-0 mt-14 md:ml-48 ">
             <div className=" font-bold text-3xl md:text-5xl leading-4 md:leading-7">
               <h1>PHARMACEUTICAL ASSOCIATION </h1>
@@ -27,7 +24,7 @@ function Hero() {
             </div>
           </div>
 
-          <div className=" md:bottom-0 w-11/12 m-auto md:m-px md:w-fit ">
+          <div className=" md:grid hidden md:bottom-0 w-11/12 m-auto md:m-px md:w-fit ">
             <div className=" w-fit">
               <Image width={500} height={500} alt="" src="/president.png" />
               <div className="w-fit relative -mt-20 ">
@@ -39,6 +36,21 @@ function Hero() {
                     PANS OAU President
                   </h2>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" md:bottom-0 grid md:hidden w-11/12 m-auto md:m-px md:w-fit ">
+          <div className=" w-fit">
+            <Image width={500} height={500} alt="" src="/president.png" />
+            <div className="w-fit relative -mt-20 ">
+              <div className="p-3  border bg-white rounded-t-lg">
+                <h1 className=" text-2xl font-bold text-fuchsia-800">
+                  Ademola Blessing
+                </h1>
+                <h2 className=" text-md  text-fuchsia-800">
+                  PANS OAU President
+                </h2>
               </div>
             </div>
           </div>
--- src/app/MeetAlumni/component/AlumniHero.tsx
+++ src/app/MeetAlumni/component/AlumniHero.tsx
@@ -10,9 +10,9 @@ function AlumniHero() {
   }
   return (
     <div className=' bg-fuchsia-800'>
-      <div className='m-auto w-11/12 md:w-8/12  pt-20 '>
+      <div className='m-auto w-11/12 md:w-8/12 pt-20 '>
         <div className='flex justify-between w-11/12 m-auto md:m-0 gap-5  md:w-10/12'>
-          <div onClick={()=>backMoves()} className='flex w-fit'>
+          <div onClick={backMoves} className='flex w-fit'>
             <CiCircleChevLeft
               className=' bg-white flex rounded-full'
               color='purple'
